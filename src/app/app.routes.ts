07710c70b19a8652dc670e3e55214010c761f184
saeed safari <saeid.elitem@gmail.com>
2024-08-23 17:10:50 +0330

    create folders and architectural pattern and implement views

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
+// app.routes.ts
 import { Routes } from '@angular/router';
+import { KanbanBoardComponent } from './views/kanban-board/kanban-board.component';
+import { AddNewTaskComponent } from './views/add-new-task/add-new-task.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: KanbanBoardComponent },
+  { path: 'new-task', component: AddNewTaskComponent },
+  { path: '**', redirectTo: '' },
+];
---
+++ src/app/views/add-new-task/add-new-task.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-new-task',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-new-task.component.html',
+  styleUrl: './add-new-task.component.scss'
+})
+export class AddNewTaskComponent {
+
+}
